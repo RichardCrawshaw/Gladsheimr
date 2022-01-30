104362b6bc21e0d118218b27c35aa9b2b9591488
Richard Crawshaw <richardcrawshaw41@gmail.com>
2022-01-30 15:21:42 +1100

    Add parameter to OpCode control parameters, to make it easier to identify which should tie up to which OpCode parameter. Package: 0.9.0-beta4.

---
+++ Cbus.Gladsheimr/Attributes/OpCodeParameterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cbus.Gladsheimr.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class OpCodeParameterAttribute : Attribute
+    {
+        public string? PropertyName { get; set; }
+        public string? TypeName { get; set; }
+        public string? Name { get; set; }
+        public string? DataType { get; set; }
+
+        public OpCodeParameterAttribute(string propertyName, string typeName, string name, string dataType)
+        {
+            this.PropertyName = propertyName;
+            this.TypeName = typeName;
+            this.Name = name;
+            this.DataType = dataType;
+        }
+    }
+}
